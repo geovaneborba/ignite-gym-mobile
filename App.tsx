f38cc7d40ff124cb3532879f4c72a81579fc50bf
Geovane Borba <geovanne@live.com>
2026-08-03 15:20:55 -0300

    feat(app): replace default boilerplate with core app setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { NativeBaseProvider } from 'native-base'
+import { Loading } from '@components/Loading'
+import { theme } from './src/theme'
+import { SignIn } from '@screens/SignIn'
+import { SignUp } from '@screens/SignUp'
+import { Routes } from '@routes/index'
+import { AuthContextProvider } from '@contexts/AuthContext'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <NativeBaseProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <AuthContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
+    </NativeBaseProvider>
+  )
+}
